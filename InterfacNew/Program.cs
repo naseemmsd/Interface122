1847b3fdf1d54264339c51494ae1630c124dfc4d
naseemmsd <naseemmehsood932@gmail.com>
2024-08-25 21:24:51 +0500

    interface

---
+++ InterfacNew/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNew
+{
+    public interface IAnimal
+    {
+        void Speak();
+        void Eat();
+        
+
+    }
+    public class Cat : IAnimal
+    {
+        public void Speak()
+        {
+
+            Console .WriteLine ("Meowwwwwwwww---");
+        }
+        public void Eat()
+        {
+            {
+
+                Console.WriteLine("Cats Like Meat");
+            }
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal ManoBilli = new Cat();
+            ManoBilli.Speak();
+            Console.WriteLine();
+
+            ManoBilli.Eat();
+            Console.ReadLine();
+        }
+    }
+   
+}
